4ddae4ec2d027dee09246d9a49e1b8d2e72bfe03
armashkaise <HT52JJu7bv>
2019-12-20 19:50:51 +0600

    Commit#2

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,32 @@ public class Main {
     public static void main(String[] args) {
         MyArrayList myArrayList = new MyArrayList();
 
-        myArrayList.add(1, 2, 3, 4, 5);
+        //добавление элементов
+        myArrayList.add(1);
+        myArrayList.add(2);
+        myArrayList.add(3);
         myArrayList.printValues();
         //System.out.println(myArrayList.count());
 
-        myArrayList.add(6, 7, 8);
+        //добавление новых элементов
+        myArrayList.add(4);
+        myArrayList.add(5);
+        myArrayList.add(6);
         myArrayList.printValues();
-       // System.out.println(myArrayList.count());
 
-        myArrayList.clear();
+        //добавление нового элемента в середину массива
+        myArrayList.add(4, 100);
+        myArrayList.printValues();
+
+        //вывод количества элементов
+        System.out.println("Количество элементов в массиве: " + myArrayList.count());
+
+        //удаление элемента
+        myArrayList.delete(2);
+        myArrayList.printValues();
+
+        //удаление всех элементов массива
+        myArrayList.deleteAll();
         myArrayList.printValues();
     }
 
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -4,25 +4,63 @@ public class MyArrayList {
     private int[] values;
     private int count = 0;
 
-    public void add(int... val){
+    public void add(int val){
 
         if (count == 0){
-            values = val;
+            values = new int[1];
+            values[0] = val;
         }
         else {
-            int[] newValues = new int[values.length + val.length];
+            int lengthNewArray = values.length + 1;
+            int[] newValues = new int[lengthNewArray];
             System.arraycopy(values, 0, newValues, 0, values.length);
-            System.arraycopy(val, 0, newValues, values.length, val.length);
+            newValues[lengthNewArray - 1] = val;
             values = newValues;
         }
-        count += val.length;
+        count++;
+    }
+
+    public void add(int index, int val){
+        if (index < 0 || index > count ) {
+            System.out.println("Недопустимый интервал");
+            return;
+        }
+
+        int lengthNewArray = values.length + 1;
+        int[] newArray = new int[lengthNewArray];
+        int[] firstArray = new int[index];
+        System.arraycopy(values, 0, firstArray, 0, index);
+        int[] lastArray = new int[count - index];
+        System.arraycopy(values, index, lastArray, 0, count - index);
+        System.arraycopy(firstArray, 0, newArray, 0, firstArray.length);
+        newArray[firstArray.length] = val;
+        System.arraycopy(lastArray, 0, newArray, firstArray.length + 1, lastArray.length);
+        values = newArray;
+        count++;
     }
 
     public int count(){
         return count;
     }
 
-    public void clear(){
+    public void delete(int index){
+        if (index < 0 || index > count - 1) {
+            System.out.println("Недопустимый интервал");
+            return;
+        }
+        int lengthNewArray = values.length - 1;
+        int[] newArray = new int[lengthNewArray];
+        int[] firstArray = new int[index];
+        System.arraycopy(values, 0, firstArray, 0, index);
+        int[] lastArray = new int[count - index];
+        System.arraycopy(values, index, lastArray, 0, count - index);
+        System.arraycopy(firstArray, 0, newArray, 0, firstArray.length);
+        System.arraycopy(lastArray, 1, newArray, firstArray.length, lastArray.length - 1);
+        values = newArray;
+        count--;
+    }
+
+    public void deleteAll(){
         int [] i = null;
         values = i;
         count = 0;
@@ -34,12 +72,12 @@ public class MyArrayList {
     }
 
     public void printValues(){
-        //int[] val = getValues();
         if (count > 0) {
             for (int i : values) {
                 System.out.print(i + " ");
             }
             System.out.println();
         }
+        else System.out.println("Массив пуст");
     }
 }
